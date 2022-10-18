c667184b8492c9ba407f0913561507b0d6693763
Aman Madd <29maddhesiaaman@gmail.com>
2022-10-18 20:01:15 +0530

    Installed ejs and created a view for home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,8 @@
 module.exports.home=function(req,res){
-    return res.end('<h1> Express is up for Codeial!</h1>');
+    // let's comment the below line because it is sending response directly to the browser
+    // return res.end('<h1> Express is up for Codeial!</h1>');
+    return res.render("home",{
+        title:"Home"
+    })
+
 }
