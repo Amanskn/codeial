f3c3fae3c1e49865b0bf39bf128f7711c4363084
Aman Madd <29maddhesiaaman@gmail.com>
2022-10-18 13:30:08 +0530

    First commit: create the directory structure and setup express

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require("express");
+const app=express();
+// default port:- 80 for http and 443 for https
+const port=5000;
+
+
+app.listen(port,function(err){
+    if(err){
+        // console.log("Error in running the server:",err);
+        // This is another way to print statements using acute symbol `
+        // This is called Interpolation
+        console.log(`Error in running the server: ${err}`);
+    }
+    console.log(`Server is running on port no: ${port}`);
+})
