d474a0b6b777d5290cbe33f2a2e6a9b760a7dd19
Aman Madd <29maddhesiaaman@gmail.com>
2022-11-06 23:56:44 +0530

    Done with decoration of header (using SCSS) but yeah footer needs to be done

--- index.js
+++ index.js
@@ -21,6 +21,18 @@ const passportLocal=require('./config/passport-local-strategy');
 // This is newer version of code
 const MongoStore=require('connect-mongo');
 
+const sassMiddleware=require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src:'./assets/scss',
+    dest:'./assets/css',
+    debug:true,
+    outputStyle:"extended",
+    prefix:'/css'
+}));
+
+
+
 app.use(express.urlencoded());
 app.use(cookieParser());
 
