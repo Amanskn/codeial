99eee41435ba212aed2a787e76b44089b5794658
Aman Madd <29maddhesiaaman@gmail.com>
2022-11-08 12:41:06 +0530

    Displayed the post and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,27 @@
+const Post= require('../models/post');
+
 module.exports.home=function(req,res){
     // let's comment the below line because it is sending response directly to the browser
     // return res.end('<h1> Express is up for Codeial!</h1>');
 
 
     // below two lines are cookie related stuffs
-    console.log(req.cookies);
-    res.cookie('user',4);
-    return res.render("home",{
-        title:"home"
-    })
+    // console.log(req.cookies);
+    // res.cookie('user',4);
+
+    // Post.find({},function(err,posts){
+    //     return res.render("home",{
+    //         title:"Codeial | Home",
+    //         posts:posts
+    //     });
+    // });
+    
+    // Populate the user of each post
+     Post.find({}).populate('user').exec(function(err,posts){
+        return res.render("home",{
+            title:"Codeial | Home",
+            posts:posts
+        });
+    });
+    
 }
